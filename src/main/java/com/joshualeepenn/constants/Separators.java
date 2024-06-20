10ffd664a7a7e166d0eea25f332a58c032871615
Joshua Penn <joshualeepenn@gmail.com>
2024-06-19 20:23:16 -0400

    initial commit

---
+++ src/main/java/com/joshualeepenn/constants/Separators.java
@@ -0,0 +1,11 @@
+package com.joshualeepenn.constants;
+
+public class Separators {
+    public static final String SPACE = " ";
+    public static final String TAB = "\t";
+    public static final String NEW_LINE = "\n";
+    public static final String COMMA = ",";
+    public static final String COMMA_SPACE = ",";
+    public static final String SEMICOLON = ";";
+    public static final String COLON = ":";
+}
