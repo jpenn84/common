967531da85ae5b8925b5dd5d3ac30c654e31249f
Joshua Penn <joshualeepenn@gmail.com>
2024-06-28 15:21:47 -0400

    fix state test with null capital

--- src/test/java/com/joshualeepenn/locations/united_states/StateTest.java
+++ src/test/java/com/joshualeepenn/locations/united_states/StateTest.java
@@ -146,10 +146,6 @@ public class StateTest {
                 State.getStateByCapital("honolulu"),
                 State.HI
         );
-        sa.assertEquals(
-                State.getStateByCapital(null),
-                State.HI
-        );
         sa.assertAll();
     }
 
